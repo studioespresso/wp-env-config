b32dd81b0e443be059a961a61e85d9ebf7edc1ae
Jan Henckens <jan@statik.be>
2013-12-23 10:32:54 +0100

    Environment files

---
+++ config/config.dev.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Development config overrides & db credentials
+ *
+ * @package    On Edge Wordpress config
+ * @version    1.0
+ * @author     On Edge <dev@onedge.be>
+ */
+
+$config_db = '';
+$config_user = '';
+$config_pw = '';
+$config_host = '';
+$config_charset = 'utf8';
+$config_tableprefix = 'wp_';
+define('DB_COLLATE', ''); 
+
+/* End of file config.dev.php */
+/* Location: ./config/config.dev.php */
